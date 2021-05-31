ba51bf4f8f7ea550d8e39f52735bb947d7f895aa
caboosecodes <ricardolopez413@hotmail.com>
2021-05-31 16:57:13 -0700

    Update lambdaChallenge.py

---
+++ lambdaChallenge.py
@@ -0,0 +1,6 @@
+#lambda function is a small anonymous function and can take any number of arguments
+#but can only have on expression
+#an expression is a combination of values that computed by the computer
+
+x = lambda a, b, c: (a*b)/c
+print(x(6,4,8))
