14a9a4aa700f0fc081644c942d807dcee32c8ec0
caboosecodes <ricardolopez413@hotmail.com>
2021-06-10 12:17:29 -0700

    generate webpage sale.html

---
+++ challenges/webpageGenerator.py
@@ -0,0 +1,17 @@
+
+
+import webbrowser
+
+f = open("sale.html", "w")
+f.write(" \
+    <html>\
+        <body>\
+            <h1>\
+        Stay tuned for our amazing summer sale!\
+            </h1>\
+        </body>\
+    </html>\
+    ")
+f.close()
+
+webbrowser.open_new_tab("sale.html")
