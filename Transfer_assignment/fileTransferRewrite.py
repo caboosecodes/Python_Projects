24f372caebd1311dff1167833d7c62e94d193c6d
caboosecodes <ricardolopez413@hotmail.com>
2021-06-14 10:57:18 -0700

    coding challenges

---
+++ Transfer_assignment/fileTransferRewrite.py
@@ -0,0 +1,33 @@
+from datetime import *
+
+from datetime import datetime
+
+import os
+
+import shutil
+
+# path to the folder where all the files are located
+source = "C:\\Users\\Ricar\\Documents\\GitHub\\Python_Projects\\Transfer_assignment\\Files"
+
+# path to where I want the files to go
+destination = "C:\\Users\\Ricar\\Documents\\GitHub\\Python_Projects\\Transfer_assignment\\Destination"
+
+# os.listdir() makes a list of all the files inside the directory in this case
+fileNames = os.listdir(source)
+print(fileNames)
+
+for file in fileNames:
+    # path for the file
+    abPath = os.path.join(source, file)
+    # gets time in a float
+    modifySec = os.path.getmtime(abPath)
+    # converts float in to datetime structure
+    modifyTime = datetime.fromtimestamp(modifySec)
+    
+    # if the modyfied date is within the last day then copy over the file
+    if modifyTime > (datetime.now() + timedelta(days=-1)):
+        shutil.copy(abPath, destination)
+
+    
+    """if datetime.fromtimestamp(file) > (datetime.now() + timedelta(days=-1)):
+        shutil.copyfiles(source, destination)"""
